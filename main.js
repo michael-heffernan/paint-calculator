4d4e70de68fe0a6f8ed95e20bd78ca7e942380ee
Michael <michael.heffernan1@sky.com>
2024-05-19 10:19:03 +0100

    Final version for submission

--- main.js
+++ main.js
@@ -5,43 +5,67 @@ const start = () => {
   paintCalc();
 };
 
+// Calculate paint needed
 const paintCalc = () => {
-  let paintNeeded = arraySum(roomCalc());
+  let areaToPaint = arraySum(roomCalc());
 
+  // Add optional 10% wastage buffer
   console.log("\nWould you like to include a 10% wastage buffer? (y/n)");
   let wastageBuffer = inputYorN();
-  paintNeeded = wastageBuffer ? paintNeeded * 1.1 : paintNeeded;
-  paintNeededLitre = Math.round(paintNeeded * 100) / 100;
-  console.log("\nPaint needed: " + paintNeededLitre + "L");
+  paintNeeded = wastageBuffer ? areaToPaint * 1.1 : areaToPaint;
+  paintNeededLitre = Math.round((paintNeeded / 1000) * 100) / 100; // Convert to litres
 
-  console.log("\n\t----------Cans needed----------");
-  const cans = canCalc(paintNeeded);
-  printTable(cans);
+  // Get desired paint type
+  console.log("What type of paint would you like?");
+  const paintType = menuInput((options = ["GoodHome", "Dulux"]));
+
+  // Print final results
+  console.log("\nTotal area to paint: " + areaToPaint / 100 + " square meters");
+  console.log("10% wastage buffer? " + wastageBuffer.toUpperCase());
+  console.log("Paint needed: " + paintNeededLitre + "L");
+  printTable((cans = canCalc(paintNeededLitre)), (type = paintType));
 };
 
-// Calculate most efficient combination of cans
-const canCalc = (paintQuant) => {
-  let cans = [0, 0, 0];
-  let remainder = paintQuant / 1000;
-  while (remainder != 0) {
-    if (remainder >= 10) {
-      let bigCans = Math.floor(remainder / 10);
-      cans[0] = bigCans;
-      remainder -= bigCans * 10;
-    } else if (remainder >= 5) {
-      let medCans = Math.floor(remainder / 5);
-      cans[1] = medCans;
-      remainder -= medCans * 5;
-    } else if (remainder >= 2.5) {
-      let smallCans = Math.floor(remainder / 2.5);
-      cans[2] = smallCans;
-      remainder -= smallCans * 2.5;
-    } else {
-      cans[2] += 1;
-      remainder = 0;
+// Display results
+const printTable = (cans, type = "Dulux") => {
+  let total = 0;
+  let canPrices;
+
+  // Find correct can price list
+  if (type == "GoodHome") {
+    canPrices = paintTypes.GoodHome.prices;
+  } else if (type == "Dulux") {
+    canPrices = paintTypes.Dulux.prices;
+  }
+  // Print results in a table
+  console.log("\n\t----------Cans needed----------");
+  console.log("Can size\t\tQty\t\tCost");
+  for (let i = 0; i < cans.length; i++) {
+    switch (i) {
+      case 0:
+        cost = cans[i] * canPrices[i];
+        if (cost != 0) {
+          console.log("10L\t\t\t" + cans[i] + "\t\t£" + cost);
+        }
+        total += cost;
+        break;
+      case 1:
+        cost = cans[i] * canPrices[i];
+        if (cost != 0) {
+          console.log("5L\t\t\t" + cans[i] + "\t\t£" + cost);
+        }
+        total += cost;
+        break;
+      case 2:
+        cost = cans[i] * canPrices[i];
+        if (cost != 0) {
+          console.log("2.5L\t\t\t" + cans[i] + "\t\t£" + cost);
+        }
+        total += cost;
+        break;
     }
   }
-  return cans;
+  console.log("\t\t\t\tTotal:\t£" + total);
 };
 
 // Get room area
@@ -98,13 +122,37 @@ const obstructionCalc = (area) => {
     } else {
       console.log("Are there any more obstructions? (y/n)");
       moreObstructionsIn = inputYorN();
-
       moreObstructions = moreObstructionsIn != "y" ? false : true;
       obstructionsAreaTotal += obstructionArea;
     }
   } while (moreObstructions == true);
 
   return obstructionsAreaTotal;
+};
+
+// Calculate most efficient combination of can sizes
+const canCalc = (paintQuant) => {
+  let cans = [0, 0, 0];
+  let remainder = paintQuant;
+  while (remainder != 0) {
+    if (remainder >= 10) {
+      let bigCans = Math.floor(remainder / 10);
+      cans[0] = bigCans;
+      remainder -= bigCans * 10;
+    } else if (remainder >= 5) {
+      let medCans = Math.floor(remainder / 5);
+      cans[1] = medCans;
+      remainder -= medCans * 5;
+    } else if (remainder >= 2.5) {
+      let smallCans = Math.floor(remainder / 2.5);
+      cans[2] = smallCans;
+      remainder -= smallCans * 2.5;
+    } else {
+      cans[2] += 1;
+      remainder = 0;
+    }
+  }
+  return cans;
 };
 
 // Validate user input is y or n
@@ -141,6 +189,31 @@ const inputNumber = () => {
   return input;
 };
 
+// Validate menu input
+const menuInput = (options = []) => {
+  let valid = false;
+
+  // Display options
+  for (let i = 0; i < options.length; i++) {
+    console.log("[" + (i + 1) + "] " + options[i]);
+  }
+  do {
+    let input = prompt(">");
+
+    // Check input is valid number with corresponding option
+    if (input >= 1 && input <= options.length) {
+      input -= 1;
+      valid = true;
+      result = options[input];
+    } else {
+      console.log(
+        "Invalid input. Please enter a number between 1 and " + options.length
+      );
+    }
+  } while (valid == false);
+  return result;
+};
+
 // Calculate sum of array
 const arraySum = (array) => {
   let sum = 0;
@@ -150,53 +223,15 @@ const arraySum = (array) => {
   return sum;
 };
 
+// Object containing paint types and their prices
+// "prices" represents cans of 10L, 5L, 2.5L respectively
 const paintTypes = {
   GoodHome: {
-    prices: [
-      { size: 5, price: 22 },
-      { size: 2.5, price: 16 },
-    ],
+    prices: [30, 22, 16],
   },
   Dulux: {
-    prices: [
-      { size: 10, price: 24 },
-      { size: 5, price: 18 },
-      { size: 2.5, price: 15 },
-    ],
+    prices: [24, 18, 15],
   },
-};
-
-const printTable = (cans) => {
-  const header = "Can size\t\tQty\t\tCost";
-  let total = 0;
-
-  console.log(header);
-  for (let i = 0; i < cans.length; i++) {
-    switch (i) {
-      case 0:
-        cost = cans[i] * paintTypes.Dulux.prices[i].price;
-        if (cost != 0) {
-          console.log("10L\t\t\t" + cans[i] + "\t\t£" + cost);
-        }
-        total += cost;
-        break;
-      case 1:
-        cost = cans[i] * paintTypes.Dulux.prices[i].price;
-        if (cost != 0) {
-          console.log("5L\t\t\t" + cans[i] + "\t\t£" + cost);
-        }
-        total += cost;
-        break;
-      case 2:
-        cost = cans[i] * paintTypes.Dulux.prices[i].price;
-        if (cost != 0) {
-          console.log("2.5L\t\t\t" + cans[i] + "\t\t£" + cost);
-        }
-        total += cost;
-        break;
-    }
-  }
-  console.log("\t\t\t\tTotal:\t£" + total);
 };
 
 start();
